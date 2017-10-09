9c821639891a5bad0dbf6a0313f6246937986b88
ZeynabMortazavi <32588889+ZeynabMortazavi@users.noreply.github.com>
2017-10-09 10:16:20 -0700

    first website upload

---
+++ MehrProject/Controllers/SmsQuizController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MehrProject.Controllers
+{
+    public class SmsQuizController : Controller
+    {
+        // GET: SmsQuiz
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MehrProject/Gruntfile.js
@@ -0,0 +1,15 @@
+﻿copy: {
+    main: {
+            expand: true,
+            cwd: "node_modules/",
+            src: [
+
+                "bootstrap/dist/css/bootstrap.min.css",
+                "bootstrap/dist/css/bootstrap.min.css.map",
+                "bootstrap/*.*",
+                "bootstrap/**/*.*"
+            ],
+
+            "dest": "server/public/vendor/"
+    };
+};
